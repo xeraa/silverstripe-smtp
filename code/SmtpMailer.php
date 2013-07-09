8456e280e6577af7a40783110ae6e7bcbf287f9b
Jakx <jackson@jdinnovation.com>
2013-07-10 11:28:32 +1200

    SmtpMailer.php line 110 overriding pass through names format handling for sender (lines 97-101). Commented it out.

--- code/SmtpMailer.php
+++ code/SmtpMailer.php
@@ -107,7 +107,7 @@ class SmtpMailer extends Mailer {
 			$this->mailer->ClearAddresses();
 			$this->mailer->AddAddress($to, ucfirst(substr($to, 0, strpos($to, '@')))); 
 			//For the recipient's name, the string before the @ from the e-mail address is used
-			$this->mailer->SetFrom($from);
+			//$this->mailer->SetFrom($from); /* This is overriding the values set in lines 97-101
 		}
 		$this->mailer->Subject = $subject;
 	}
