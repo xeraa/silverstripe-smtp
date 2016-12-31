e88c19b933f0688b81428e11122708c055d5880e
Philipp Krenn <pk@xeraa.net>
2016-12-31 04:18:52 +0100

    Change do not overwrite the from we’ve just set

--- code/SmtpMailer.php
+++ code/SmtpMailer.php
@@ -122,7 +122,6 @@ class SmtpMailer extends Mailer {
 			$this->mailer->ClearAddresses();
 			$this->mailer->AddAddress($to, ucfirst(substr($to, 0, strpos($to, '@')))); 
 			//For the recipient's name, the string before the @ from the e-mail address is used
-			$this->mailer->SetFrom($from);
 		}
 		$this->mailer->Subject = $subject;
 	}
